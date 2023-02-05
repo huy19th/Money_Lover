2e6aee72dbcc51ec504f6653fa0cbe2a205da83c
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-02-05 09:54:04 +0700

    write app as object

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -11,49 +11,60 @@ import TestRouter from "./test.router";
 import AppConfig from "./config/app.config";
 import passport from 'passport';
 
-class App {
+
+const app = express();
+let appConfig = new AppConfig();
+
+app.use('/test', TestRouter);
+app.use((req, res) => {res.status(200).json({message: "not found"})})
+app.listen(appConfig.port, () => {
+    console.log(`server started at http://localhost:${appConfig.port}`)
+})
+
+
+// class App {
     
-    private app: express.Application = express();
+//     private app: express.Application = express();
 
-    private appConfig = new AppConfig();
+//     private appConfig = new AppConfig();
 
-    constructor() {
-        this.bootstrap();
-    }
+//     constructor() {
+//         this.bootstrap();
+//     }
 
-    public bootstrap() : void {
-        this.setupMiddlewares();
-        //this.serveStaticFiles();
-        this.listen();
+//     public bootstrap() : void {
+//         this.setupMiddlewares();
+//         //this.serveStaticFiles();
+//         this.listen();
     
-    }
+//     }
 
-    //Static  files
-    /* private serveStaticFiles(): void {
-        this.app.use(express.static(path.join(__dirname, 'FileName'), { maxAge:  this.appConfig.expiredStaticFiles}));
-    } */ 
+//     //Static  files
+//     /* private serveStaticFiles(): void {
+//         this.app.use(express.static(path.join(__dirname, 'FileName'), { maxAge:  this.appConfig.expiredStaticFiles}));
+//     } */ 
 
-    private setupMiddlewares(): void {
-        // this.app.use(bodyParser.urlencoded({ extended: true }));
-        // this.app.use(bodyParser.json());
-        // this.app.use(passport.initialize());
-        //this.app.use(passport.session());
-        // this.app.use('/auth', AuthRouter);
-        // this.app.use('/user', UserRouter);
-        // this.app.use('/transaction', TransactionRouter);
-        // this.app.use(TransCateRouter);
-        // this.app.use(TransTypeRouter);
-        // this.app.use(WalletRouter);
-        this.app.use('/test', TestRouter);
-        this.app.use((req, res) => {res.status(200).json({message: "not found"})})
-    }
+//     private setupMiddlewares(): void {
+//         // this.app.use(bodyParser.urlencoded({ extended: true }));
+//         // this.app.use(bodyParser.json());
+//         // this.app.use(passport.initialize());
+//         //this.app.use(passport.session());
+//         // this.app.use('/auth', AuthRouter);
+//         // this.app.use('/user', UserRouter);
+//         // this.app.use('/transaction', TransactionRouter);
+//         // this.app.use(TransCateRouter);
+//         // this.app.use(TransTypeRouter);
+//         // this.app.use(WalletRouter);
+//         this.app.use('/test', TestRouter);
+//         this.app.use((req, res) => {res.status(200).json({message: "not found"})})
+//     }
 
-    private listen(): void {
-        this.app.listen(this.appConfig.port, () => {
-            console.log(`server started at http://localhost:${this.appConfig.port}`);
-        });
-    }
-}
+//     private listen(): void {
+//         this.app.listen(this.appConfig.port, () => {
+//             console.log(`server started at http://localhost:${this.appConfig.port}`);
+//         });
+//     }
+// }
 
-new App();
+// new App();
 
