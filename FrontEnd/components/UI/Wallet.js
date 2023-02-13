984a05a14b0897f851c74200248a01c42ee4a498
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-02-13 11:33:35 +0700

    wallet list layout

--- FrontEnd/components/UI/Wallet.js
+++ FrontEnd/components/UI/Wallet.js
@@ -1,10 +1,13 @@
 import NavBar from "@/components/shares/NavBar";
+import WalletList from "@/components/UI/Wallet/WalletList";
+import Container from 'react-bootstrap/Container';
+
 
 export default function Wallet() {
     return (
-        <>
+        <div className="d-flex justify-content-center bg-secondary bg-opacity-25" style={{height: "100vh"}}>
             <NavBar/>
-            <h1>hello</h1>
-        </>
+            <WalletList />
+        </div>
     )
 }
---
+++ FrontEnd/components/UI/Wallet/WalletList.js
@@ -0,0 +1,22 @@
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { useSelector } from 'react-redux';
+import store from '@/store/store';
+
+export default function ListGroupWithHeaderExample() {
+
+    const user = useSelector(state => state.auth).currentUser;
+
+    return (
+        <div className="mt-5 pt-5">
+            <Card style={{ width: '18rem' }}>
+                <Card.Header>Featured</Card.Header>
+                <ListGroup variant="flush">
+                    <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                    <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                </ListGroup>
+            </Card>
+        </div>
+    );
+}
