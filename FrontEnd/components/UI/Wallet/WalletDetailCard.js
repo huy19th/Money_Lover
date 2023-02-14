f5af98bfe2a2b73b72293a8f7d237aada2880c1a
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-02-14 20:35:49 +0700

    add wallet detail card

--- FrontEnd/components/UI/Wallet/WalletDetailCard.js
+++ FrontEnd/components/UI/Wallet/WalletDetailCard.js
@@ -2,6 +2,9 @@ import Button from "@mui/material/Button";
 import Card from 'react-bootstrap/Card';
 import CloseIcon from '@mui/icons-material/Close';
 import { useState } from 'react';
+import { Divider, Checkbox } from '@mui/material';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 function WalletDetailCard() {
     const myHandleCLose = () => {
@@ -23,7 +26,7 @@ function WalletDetailCard() {
     }
 
     const handleOver = (event) => {
-        event.target.classList.add(styles.changePointer)
+
     }
 
     return (
@@ -31,7 +34,7 @@ function WalletDetailCard() {
             <Card.Header>
                 <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
                     <div style={{ display: "flex", alignItems: "center" }}>
-                        <CloseIcon className='ms-2' onClick={myHandleCLose} onMouseOver={(event) => handleOver(event)} />
+                        <CloseIcon className='ms-3' onClick={myHandleCLose} onMouseOver={(event) => handleOver(event)} />
                         <p className='m-0 ms-3' style={{ fontWeight: "bold", fontSize: '16px' }}>Wallet Details</p>
                     </div>
                     <div style={{ display: "flex", alignItems: "center" }}>
@@ -40,12 +43,30 @@ function WalletDetailCard() {
                     </div>
                 </div>
             </Card.Header>
-            <Card.Body>
-                <Card.Title>Special title treatment</Card.Title>
-                <hr/>
-                <Card.Text>
-                    With supporting text below as a natural lead-in to additional content.
-                </Card.Text>
+            <Card.Body className="px-0">
+                <div className="ms-5 mb-3 d-flex align-items-center">
+                    <img src="https://static.moneylover.me/img/icon/icon.png" style={{ height: "56px", width: "56px" }} />
+                    <div className="d-inline-flex flex-column ms-3">
+                        <h4>Bank Acount</h4>
+                        <span className="text-secondary">Việt Nam Đồng</span>
+                    </div>
+                </div>
+                <div className="ms-5 mb-3 d-flex align-items-center">
+                    <div className="d-inline-flex flex-column ms-3">
+                        <Checkbox {...label} defaultChecked color="success" />
+                    </div>
+                    <div className="d-inline-flex flex-column ms-3">
+                        <span>Excluded from Total</span>
+                        <span className="text-secondary" style={{"font-size": "12px"}}>
+                            Ignore this wallet and its balance in the "Total" mode.
+                        </span>
+                    </div>
+                    <div>
+                        <Checkbox {...label} defaultChecked color="success" />
+                    </div>
+
+                </div>
+                <Divider />
                 <Button variant="primary">Go somewhere</Button>
             </Card.Body>
         </Card>
