6721de24d8e7de6c4eb80a84aec3b48dc4eefdbf
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-02-15 21:07:04 +0700

    add api update wallet

--- BackEnd/src/controllers/wallet.controller.ts
+++ BackEnd/src/controllers/wallet.controller.ts
@@ -1,12 +1,12 @@
 import BaseController from "./base.controller";
 
-import {Request, Response} from "express";
-import WalletModel, {Wallet} from "../models/wallet.model";
+import { Request, Response } from "express";
+import WalletModel, { Wallet } from "../models/wallet.model";
 
 import User from "../models/user.model"
 import dataSource from "../database/data-source";
 import transactionController from "./transaction.controller";
-import TransactionModel, {Transaction} from "../models/transaction.model";
+import TransactionModel, { Transaction } from "../models/transaction.model";
 import SubCate from "../models/trans.subcate.model";
 import transactionType from "../models/trans.type.model";
 import TransactionServices from "../services/transaction.services";
@@ -20,16 +20,16 @@ let transactionTypeRepo = dataSource.getRepository(transactionType);
 
 class WalletController extends BaseController {
 
-    static getAllWalletsOfUser (req: Request, res: Response) {
+    static getAllWalletsOfUser(req: Request, res: Response) {
         //@ts-ignore
         let userId = req.user.id;
         WalletServices.getAllWalletsOfUser(userId)
-        .then(wallets => {
-            res.status(200).json(wallets);
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        })
+            .then(wallets => {
+                res.status(200).json(wallets);
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            })
     }
 
     static getDetailInfoOfWallet(req: Request, res: Response) {
@@ -52,23 +52,23 @@ class WalletController extends BaseController {
     static getWallet(req: Request, res: Response) {
         let walletId = Number(req.params.walletId);
         WalletServices.getWalletById(walletId)
-        .then(wallet => {
-            res.status(200).json(wallet);
-        })
-        .catch(err => {
-            res.status(500).json({message: err.message || this.defaultErrorMessage});
-        })
+            .then(wallet => {
+                res.status(200).json(wallet);
+            })
+            .catch(err => {
+                res.status(500).json({ message: err.message || this.defaultErrorMessage });
+            })
     }
 
     static async adjustBalance(req: Request, res: Response) {
         try {
-            let {walletId, balance} = req.body;
+            let { walletId, balance } = req.body;
             await TransactionServices.addTransactionToAdjustBalance(walletId, balance);
             await WalletServices.updateBalance(walletId);
             res.status(200).json("Adjusted balance succesfully!");
         }
         catch (err) {
-            res.status(500).json({message: err.message || this.defaultErrorMessage});
+            res.status(500).json({ message: err.message || this.defaultErrorMessage });
         }
     }
 
@@ -85,12 +85,12 @@ class WalletController extends BaseController {
     static getTotalIncomeExpenseOfWallet(req: Request, res: Response) {
         let walletId = Number(req.params.walletId);
         WalletServices.getTotalIncomeExpenseOfWallet(walletId)
-        .then(result => {
-            res.status(200).json(result);
-        })
-        .catch(err => {
-            res.status(500).json(err.message);
-        })
+            .then(result => {
+                res.status(200).json(result);
+            })
+            .catch(err => {
+                res.status(500).json(err.message);
+            })
     }
 
     static getWalletsByIncludedIntotal(req: Request, res: Response) {
@@ -98,12 +98,23 @@ class WalletController extends BaseController {
         let userId = req.user.id;
         let isIncluded = req.params.isIncluded == "true" ? true : false;
         WalletServices.getWalletsByIncludedInTotal(userId, isIncluded)
-        .then(wallets => {
-            res.status(200).json(wallets);
-        })
-        .catch(err => {
-            res.status(500).json(err.message || this.defaultErrorMessage);
-        })
+            .then(wallets => {
+                res.status(200).json(wallets);
+            })
+            .catch(err => {
+                res.status(500).json(err.message || this.defaultErrorMessage);
+            })
+    }
+
+    static async updateWallet(req: Request, res: Response) {
+        try {
+            await WalletServices.updateWallet(req.body);
+            await WalletServices.updateBalance(req.body.walletId);
+            res.status(200).json("Update wallet successfully!");
+        }
+        catch (err) {
+            res.status(500).json({ message: err.message || this.defaultErrorMessage });
+        }
     }
 }
 
--- BackEnd/src/routers/wallet.router.ts
+++ BackEnd/src/routers/wallet.router.ts
@@ -10,6 +10,6 @@ WalletRouter.patch('/balance', WalletController.adjustBalance);
 WalletRouter.get('/balance/total', WalletController.getTotalBalance);
 WalletRouter.get('/:walletId/income-expense', WalletController.getTotalIncomeExpenseOfWallet);
 WalletRouter.get('/included-in-total/:isIncluded(true|false)', WalletController.getWalletsByIncludedIntotal);
-
+WalletRouter.patch('/', WalletController.updateWallet);
 
 export default WalletRouter;
