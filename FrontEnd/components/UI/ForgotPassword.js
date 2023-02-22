2538a57f3b919ab421b82d82a5583513941dbaca
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2023-02-22 14:14:03 +0700

    add notification

--- FrontEnd/components/UI/ForgotPassword.js
+++ FrontEnd/components/UI/ForgotPassword.js
@@ -15,13 +15,9 @@ import UserService from '@/services/user.service';
 
 const ForgotPassword = () => {
 
-    const dispatch = useDispatch();
-
     const router = useRouter;
 
     const [open, setOpen] = useState(false);
-
-    const [disabled, setDisabled] = useState(false);
 
     const [snackbar, setSnackbar] = useState({
         severity: "",
@@ -36,48 +32,62 @@ const ForgotPassword = () => {
                 .email('Invalid email address')
                 .required('Email is required'),
         }), onSubmit: values => {
-            UserService.forgotPassword(values);
-            setDisabled(true);
+            UserService.forgotPassword(values)
+                .then(res => {
+                    console.log(res);
+                    setOpen(true);
+                    setSnackbar({
+                        severity: "success",
+                        message: res.data.message
+                    });
+                })
+                .catch(err => {
+                    setOpen(true);
+                    setSnackbar({
+                        severity: "error",
+                        message: err.response.data.message
+                    });
+                })
         }
     })
     return (
-            <MDBContainer style={{ height: '100vh', backgroundColor: 'lightgray' }} fluid>
-                <MDBRow className='d-flex justify-content-center align-items-center'>
-                    <MDBCol style={{ backgroundColor: 'lightgray', height: "100vh", padding: "0px" }} col='12'>
-                        <div style={{ backgroundColor: '#00710f', height: '350px', width: "100%" }}>
-                            <Logo />
-                        </div>
-                        <MDBCard className='bg-white mx-auto'
-                            style={{ borderRadius: '1rem', maxWidth: '500px', zIndex: 999, top: "-185px" }}>
-                            <MDBCardBody className='px-5 py-5 w-100 d-flex flex-column'>
-                                <MDBRow>
-                                    <h2 className="fw-bold mb-3 text-center">Forgot Password</h2>
-                                    <MDBCol className="mt-4">
-                                        <form onSubmit={formik.handleSubmit}>
-                                            <p>Enter the email address you used to register, and we will send you an email to recover your password in no time.</p>
-                                            <MDBInput wrapperClass='my-2 w-100' placeholder='Email' id='email'
-                                                type='email' name='email' onChange={formik.handleChange}
-                                                value={formik.values.email} size="lg" autoFocus />
-                                            {formik.errors.email && formik.touched.email && (<p style={{ color: 'red' }}>{formik.errors.email}</p>)}
-                                            <br />
-                                            <Button type='submit' style={{ marginTop: '10px', width: '100%' }} variant="success" size='lg' disabled={disabled}>
-                                                Confirm
-                                            </Button>
-                                            <br />
-                                            <p style={{ textAlign: 'center', textDecoration: 'none', marginTop: '10px' }}>
-                                                <Link href="/login">Back to Login</Link>
-                                                {" or "}
-                                                <Link href="/register">Create Account</Link>
-                                            </p>
-                                        </form>
-                                    </MDBCol>
-                                </MDBRow>
-                            </MDBCardBody>
-                        </MDBCard>
-                    </MDBCol>
-                </MDBRow>
-                <SnackBar open={open} setOpen={setOpen} severity={snackbar.severity} message={snackbar.message} />
-            </MDBContainer>
+        <MDBContainer style={{ height: '100vh', backgroundColor: 'lightgray' }} fluid>
+            <MDBRow className='d-flex justify-content-center align-items-center'>
+                <MDBCol style={{ backgroundColor: 'lightgray', height: "100vh", padding: "0px" }} col='12'>
+                    <div style={{ backgroundColor: '#00710f', height: '350px', width: "100%" }}>
+                        <Logo />
+                    </div>
+                    <MDBCard className='bg-white mx-auto'
+                        style={{ borderRadius: '1rem', maxWidth: '500px', zIndex: 999, top: "-185px" }}>
+                        <MDBCardBody className='px-5 py-5 w-100 d-flex flex-column'>
+                            <MDBRow>
+                                <h2 className="fw-bold mb-3 text-center">Forgot Password</h2>
+                                <MDBCol className="mt-4">
+                                    <form onSubmit={formik.handleSubmit}>
+                                        <p>Enter the email address you used to register, and we will send you an email to recover your password in no time.</p>
+                                        <MDBInput wrapperClass='my-2 w-100' placeholder='Email' id='email'
+                                            type='email' name='email' onChange={formik.handleChange}
+                                            value={formik.values.email} size="lg" autoFocus />
+                                        {formik.errors.email && formik.touched.email && (<p style={{ color: 'red' }}>{formik.errors.email}</p>)}
+                                        <br />
+                                        <Button type='submit' style={{ marginTop: '10px', width: '100%' }} variant="success" size='lg'>
+                                            Confirm
+                                        </Button>
+                                        <br />
+                                        <p style={{ textAlign: 'center', textDecoration: 'none', marginTop: '10px' }}>
+                                            <Link href="/login">Back to Login</Link>
+                                            {" or "}
+                                            <Link href="/register">Create Account</Link>
+                                        </p>
+                                    </form>
+                                </MDBCol>
+                            </MDBRow>
+                        </MDBCardBody>
+                    </MDBCard>
+                </MDBCol>
+            </MDBRow>
+            <SnackBar open={open} setOpen={setOpen} severity={snackbar.severity} message={snackbar.message} />
+        </MDBContainer>
     );
 }
 export default ForgotPassword;
